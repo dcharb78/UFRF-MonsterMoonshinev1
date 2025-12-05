2052e348101681e370ce38dd78655f1fbd425d00
Daniel Charbonneau <your.email@example.com>
2025-12-04 19:11:45 -0600

    Update lakefile.lean to include ZPartition

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.26.0-rc2"
 
 lean_lib «MonsterMoonshineUFRF» where
-  roots := #[`Monster_Moonshine, `PhaseLog_Monoid, `Concurrency_BoundedGap]
+  roots := #[`Monster_Moonshine, `PhaseLog_Monoid, `Concurrency_BoundedGap, `ZPartition]
